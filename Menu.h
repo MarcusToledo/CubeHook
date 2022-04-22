6234fe14725fb248531c287a67ff5e806c986a38
s0rps <marcustoledo1337@gmail.com>
2022-04-22 09:17:09 -0300

    Implemente universal hook function for the dx9 Implemented menu with imgui

---
+++ Menu.h
@@ -0,0 +1,51 @@
+#pragma once
+
+//get game window_class_name with tools such as AutoIt3Info.exe
+static constexpr const char* GAME_WINDOW_CLASS_NAME = "SDL_app";
+
+struct WindowRect {
+	int32_t m_WindowXPos, m_WindowYPos, m_WindowWidth, m_WindowHeight;
+
+	WindowRect(int32_t windowXPos, int32_t windowYPos, int32_t windowWidth, int32_t windowHeight) {
+		this->m_WindowXPos = windowXPos;
+		this->m_WindowYPos = windowYPos;
+		this->m_WindowWidth = windowWidth;
+		this->m_WindowHeight = windowHeight;
+	}
+};
+
+class Menu {
+public:
+	Menu() = default;
+	~Menu() = default;
+	static void handleMessages() noexcept;
+
+	void renderDirectXFrame() noexcept;
+
+	void initilizeWindow(HMODULE hInstance, const char* windowClassName, const char* windowName) noexcept;
+
+
+	HWND getOverlayWindowHandle()						const noexcept { return this->m_OverlayWindowHandle; }
+	HWND getGameWindowHandle()							const noexcept { return this->m_GameWindowHandle; }
+	WNDPROC getGameWindowProc()							const noexcept { return this->m_GameWindowProc; }
+	D3DPRESENT_PARAMETERS* getD3DPresentParameters()	noexcept { return &this->m_D3D9PresentParams; }
+	bool IsMenuOpened()									const noexcept { return this->m_MenuOpened; }
+
+	void setMenuOpenedBool(bool opened)					noexcept { this->m_MenuOpened = opened; }
+private:
+	void renderImGuiWindow()	noexcept;
+private:
+	HWND m_OverlayWindowHandle;
+	HWND m_GameWindowHandle;
+	WNDPROC m_GameWindowProc;
+	D3DPRESENT_PARAMETERS m_D3D9PresentParams;
+	const char* m_WindowClassName;
+	const char* m_WindowName;
+	DWORD m_GameProcessId;
+	HMODULE m_HInstance;
+	LPDIRECT3DDEVICE9EX m_D3D9Device;
+	bool m_MenuOpened = true;
+};
+
+extern Menu GMenu;
+
--- pch.h
+++ pch.h
@@ -6,6 +6,22 @@
 
 #ifndef PCH_H
 #define PCH_H
+#define WIN32_LEAN_AND_MEANS
+
+#include <cstdio>
+#include <iostream>
+
+#include <d3d9.h>
+#include <d3dx9.h>
+#pragma comment(lib, "d3d9.lib")
+#pragma comment(lib, "d3dx9.lib")
+
+//ImGui files
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_dx9.h"
+#include "imgui/imgui_impl_win32.h"
+#include "imgui/imgui_internal.h"
+
 
 // add headers that you want to pre-compile here
 #include "framework.h"
